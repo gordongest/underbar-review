921ab0f00461c08f67a06597332a184d2b556090
gordongest <gordon.gest@gmail.com>
2020-10-06 21:23:16 -0500

    every, some, extend, defaults complete

--- spec/part2.js
+++ spec/part2.js
@@ -183,9 +183,12 @@
       });
 
       it('should override properties found on the destination', function() {
-        // Replace this line with an `expect` statement that tests
-        // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+
+        var destination = {a: 1, b: 2};
+        var source = {a: 3, c: 4};
+        var expected = _.extend(destination, source);
+
+        expect(expected.a).to.equal(3);
       });
 
       it('should not override properties not found in the source', function() {
@@ -263,7 +266,13 @@
       it('should copy any property whose key is not already set on the target', function() {
         // Replace this line with an `expect` statement that tests
         // the behavior described by the `it` string
-        throw new Error('This test is missing.');
+        var destination = {a: 1, b: 2};
+        var source = {c: 3};
+
+        _.defaults(destination, source);
+
+        expect(destination.c).to.equal(3);
+
       });
 
       it('should not copy a property if that key is already set on the target', function() {
